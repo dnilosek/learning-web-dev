fd1a3054cc93016afd1520742d96bd67d0779bc1
Dave Nilosek <david.nilosek@eagleview.com>
2019-07-24 05:56:33 -0700

    added cookies

---
+++ code/027-cookies/04_max_Age/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func main() {
+	http.HandleFunc("/", set)
+	http.HandleFunc("/count", count)
+	http.HandleFunc("/reset", reset)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func set(res http.ResponseWriter, req *http.Request) {
+	// increment counter
+	cookie := incCookie(req)
+	http.SetCookie(res, cookie)
+}
+
+func count(res http.ResponseWriter, req *http.Request) {
+	// count
+	cookie := incCookie(req)
+	http.SetCookie(res, cookie)
+
+	fmt.Fprintf(res, "You've visited %v times", cookie.Value)
+}
+
+func incCookie(req *http.Request) *http.Cookie {
+	cookie, err := req.Cookie("counter")
+
+	if err == http.ErrNoCookie {
+		cookie = &http.Cookie{
+			Name:  "counter",
+			Value: "0",
+			Path:  "/",
+		}
+	}
+
+	count, err := strconv.Atoi(cookie.Value)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	count++
+	cookie.Value = strconv.Itoa(count)
+	return cookie
+}
+
+func reset(res http.ResponseWriter, req *http.Request) {
+	cookie, err := req.Cookie("counter")
+	if err != nil {
+		http.Redirect(res, req, "/count", http.StatusSeeOther)
+		return
+	}
+	cookie.MaxAge = -1 // delete cookie
+	http.SetCookie(res, cookie)
+	http.Redirect(res, req, "/count", http.StatusSeeOther)
+}

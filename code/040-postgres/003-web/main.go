ff494a25f0e843252dd78184968cd4f5e590bb5b
Dave Nilosek <david.nilosek@eagleview.com>
2019-08-01 22:29:37 -0700

    crud

--- code/040-postgres/003-web/main.go
+++ code/040-postgres/003-web/main.go
@@ -31,9 +31,39 @@ func init() {
 
 func main() {
 	http.HandleFunc("/books", index)
+	http.HandleFunc("/books/show", isbn)
 	http.ListenAndServe(":8080", nil)
 }
 
+func isbn(res http.ResponseWriter, req *http.Request) {
+	if req.Method != "GET" {
+		http.Error(res, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+	isbn := req.FormValue("isbn")
+	if isbn == "" {
+		http.Error(res, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+	row := db.QueryRow(`SELECT * FROM books WHERE isbn = $1;`, isbn)
+
+	bk := Book{}
+	err := row.Scan(&bk.ISBN, &bk.Title, &bk.Author, &bk.Price) // order matters
+	switch {
+	case err == sql.ErrNoRows:
+		http.NotFound(res, req)
+		return
+	case err != nil:
+		http.Error(res, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	bkJson, err := json.Marshal(bk)
+	if err != nil {
+		http.Error(res, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(res, "%s\n", bkJson)
+}
 func index(res http.ResponseWriter, req *http.Request) {
 	if req.Method != "GET" {
 		http.Error(res, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
